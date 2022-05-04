97fcde3ee6a06b9fe225c68c796d67113dda28b9
Marcell Tanure <paganini.marcell@gmail.com>
2022-05-04 16:28:52 -0300

    State hook reimplemented to ship multiple values

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 import './App.css';
 import FormInput from './Components/FormInput';
 
 function App() {
-  //const [userName, setUserName] = useState("");
-  const userNameRef = useRef();
+  const [values, setValues] = useState({ userName: "",
+                                         email: "",
+                                         birthday: "",
+                                         password: "",
+                                         confirmPassword: "" });
+
+  const inputs = [
+    { id: 1,
+      name: "userName",
+      type: "text",
+      placeholder: "User Name",
+      label: "User Name"
+    },
+    { id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email"
+    },
+    { id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday"
+    },
+    { id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password"
+    },
+    { id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      label: "Confirm Password"
+    }
+  ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(e.target)
-    console.log(Object.fromEntries(data.entries()));
   };
 
-  console.log(userNameRef)
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+
+  console.log(values);
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <FormInput name="userName" placeholder="User Name" refer={userNameRef} />
-        <FormInput name="email" placeholder="Email" />
-        <FormInput name="fullName" placeholder="Full Name" />
-        <FormInput name="phone" placeholder="Phone Number" />  
+        {inputs.map((input) => (
+          <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange} />
+        ))}
         <button>Submit</button>
       </form>
     </div>
--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './FormInput.css'
 
 export default function FormInput(props) {
+  const { label, onChange, id, ...inputProps } = props;  
   return (
     <div className="form-input">
-        {/*<label>User Name</label>*/}
-        <input name={props.name} placeholder={props.placeholder} ref={props.refer}/>
+        <label>{label}</label>
+        <input {...inputProps} onChange={onChange}/>
     </div>
   )
 }
