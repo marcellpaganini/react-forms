e7fe643f652cd4033832d6b941b090da95df7ca9
Marcell Tanure <paganini.marcell@gmail.com>
2022-05-05 17:10:54 -0300

    validation logic added

--- src/App.js
+++ src/App.js
@@ -15,34 +15,42 @@ function App() {
       type: "text",
       placeholder: "User Name",
       errorMessage: "User name should be 3-16 characters and shouldn't include any special chatacter.",
-      label: "User Name"
+      label: "User Name",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true
     },
     { id: 2,
       name: "email",
       type: "email",
       placeholder: "Email",
       errorMessage: "It should be a valid email address.",
-      label: "Email"
+      label: "Email",
+      required: true
     },
     { id: 3,
       name: "birthday",
       type: "date",
       placeholder: "Birthday",
-      label: "Birthday"
+      label: "Birthday",
+      required: true
     },
     { id: 4,
       name: "password",
       type: "password",
       placeholder: "Password",
       errorMessage: "Password should be 8-20 chatacters and include at least 1 character, 1 number and 1 special character.",
-      label: "Password"
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      label: "Password",
+      required: true
     },
     { id: 5,
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm Password",
       errorMessage: "Passwords don't match.",
-      label: "Confirm Password"
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true
     }
   ];
 
