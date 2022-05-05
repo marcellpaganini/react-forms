1faa0f9731e38a1d3a356360993a9863432ee76a
paganini.marcell <paganini.marcell@gmail.com>
2022-05-05 18:58:46 -0300

    Validation msgs hidden

--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import { React, useState} from 'react'
 import './FormInput.css'
 
 export default function FormInput(props) {
-  const { label, errorMessage, onChange, id, ...inputProps } = props;  
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+  
+  const handleFocus = (e) => {
+    setFocused(true);  
+  }
+
   return (
     <div className="formInput">
         <label>{label}</label>
-        <input {...inputProps} onChange={onChange}/>
+        <input {...inputProps} onChange={onChange} onBlur={handleFocus} focused={focused.toString()} />
         <span>{errorMessage}</span>
     </div>
   )
