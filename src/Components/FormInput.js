74ad83ac2a6d1817ee2b0809ddeb130177e23ef7
paganini.marcell <paganini.marcell@gmail.com>
2022-05-04 21:12:50 -0300

    styling done

--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -4,7 +4,7 @@ import './FormInput.css'
 export default function FormInput(props) {
   const { label, onChange, id, ...inputProps } = props;  
   return (
-    <div className="form-input">
+    <div className="formInput">
         <label>{label}</label>
         <input {...inputProps} onChange={onChange}/>
     </div>
